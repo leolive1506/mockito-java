3cc519990fbfc83f63193f9fc518e55d45f8238e
Leonardo Lopes <leonardo.santana@devsquad.com>
2024-07-05 09:56:24 -0300

    handle return (stubs)

--- src/test/java/org/example/CadastrarPessoaTest.java
+++ src/test/java/org/example/CadastrarPessoaTest.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -19,7 +20,7 @@ public class CadastrarPessoaTest {
 
     @Test
     void validarDadosCadastro() {
-        var cep = "123123";
+        var cep = "1231f23";
         Mockito
                 .when(apiDosCorreios.buscarDadosPorCep(cep))
                 .thenReturn(new DadosLocalizacao("MG", "Cidade", "rua teste", "apto", "centro"));
@@ -30,5 +31,21 @@ public class CadastrarPessoaTest {
         assertEquals("MG", pessoa.getEndereco().getUf());
         assertEquals("Cidade", pessoa.getEndereco().getCidade());
         assertEquals("rua teste", pessoa.getEndereco().getLogradouro());
+    }
+
+    @Test
+    void lancarExeception() {
+        Mockito.when(apiDosCorreios.buscarDadosPorCep(ArgumentMatchers.anyString())).thenThrow(IllegalArgumentException.class);
+
+        assertThrows(IllegalArgumentException.class, () -> cadastrarPessoa.execute("leonardo", "12341234", LocalDate.now(), "1234"));
+    }
+
+    @Test
+    void lancarExeception2() {
+        Mockito.doThrow(IllegalArgumentException.class)
+            .when(apiDosCorreios)
+            .buscarDadosPorCep(ArgumentMatchers.anyString());
+
+        assertThrows(IllegalArgumentException.class, () -> cadastrarPessoa.execute("leonardo", "12341234", LocalDate.now(), "1234"));
     }
 }
